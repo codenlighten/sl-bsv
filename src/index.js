37611f09720766135dccd2e392507d6f33c351e3
SmartLedger Security <security@smartledger.com>
2025-03-07 08:12:01 -0500

    fix: export Mnemonic implementation in main package exports

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const bsv = require('bsv');
 const Point = require('./crypto/point');
+const Mnemonic = require('./mnemonic');
 
 // Create a secure fork by replacing the vulnerable crypto components
+// and adding our BSV-compatible mnemonic implementation
 const secureLib = {
   ...bsv,
   crypto: {
     ...bsv.crypto,
     Point: Point  // Replace with our secure implementation
-  }
+  },
+  Mnemonic: Mnemonic  // Add our BSV-compatible mnemonic implementation
 };
 
 module.exports = secureLib;
